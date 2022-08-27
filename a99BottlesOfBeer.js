162ad2b7542840615e1339a32720f0e2f4cb349f
Siddu-P <75543804+Siddu-P@users.noreply.github.com>
2022-08-27 19:49:24 +0530

    Create a99BottlesOfBeer.js

---
+++ a99BottlesOfBeer.js
@@ -0,0 +1,15 @@
+var numberOfBottles = 99
+while (numberOfBottles >= 0) {
+    var bottleWord = "bottle";
+    if (numberOfBottles === 1) {
+        bottleWord = "bottles";
+    } 
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+    console.log(numberOfBottles + " " + bottleWord + " of beer,");
+    console.log("Take one down, pass it around,");
+	numberOfBottles--;
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+}
+
+//99 bottles of beer on the wall, 99 bottles of beer.
+//Take one down and pass it around,98 bottles of beer on the wall.
